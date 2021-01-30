d58f5f6fa78469e483ac0d13cdc15f231aca8073
Alesandro Fidelis <BRHBS0490@ambev.com.br>
2021-01-30 14:36:11 -0300

    initial commit

---
+++ kafka-producer/Program.cs
@@ -0,0 +1,35 @@
+﻿using Confluent.Kafka;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace kafka_producer
+{
+    public static class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var config = new ProducerConfig { BootstrapServers = "localhost:9092" };
+
+            using (var produce = new ProducerBuilder<Null, string>(config).Build())
+            {
+                try
+                {
+                    var count = 0;
+                    while (true)
+                    {
+                        var dr = await produce.ProduceAsync("test-topic", new Message<Null, string> { Value = $"testando kafka: {count++}" });
+                        Console.WriteLine($"Delivered '{dr.Value}' to '{dr.TopicPartitionOffset}' | {count}");
+
+                        Thread.Sleep(2000);
+
+                    }
+                }
+                catch (ProduceException<Null, string> ex)
+                {
+                    Console.WriteLine($"Delivered failed '{ex.Error.Reason}'");
+                }
+            }
+        }
+    }
+}
